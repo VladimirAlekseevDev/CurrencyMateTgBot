7280cd5c0d275b908f7c20758c2fe6177cfe3320
Vladimir Alekseev <vladimir.alekseev.dev@gmail.com>
2025-04-04 18:19:34 +0400

    Fixed ApplicationContextLoadsTest

--- src/test/java/dev/sgd/currencymate/tgbot/ApplicationContextLoadsTest.java
+++ src/test/java/dev/sgd/currencymate/tgbot/ApplicationContextLoadsTest.java
@@ -1,10 +1,16 @@
 package dev.sgd.currencymate.tgbot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ApplicationContextLoadsTest {
+
+    @MockBean
+    private CommandLineRunner commandLineRunner;
 
     @Test
     void contextLoads() {
